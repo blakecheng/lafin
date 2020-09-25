596765aca824f676f903027c8a2e2645975b6c7c
c50012723 <chengbin17@huawei.com>
2020-09-25 09:45:15 -0400

    add styleganbase ae

--- src/lafin.py
+++ src/lafin.py
@@ -300,7 +300,7 @@ class Lafin():
                     for i in range(landmarks.shape[0]):
                         landmark_map[i,0,landmarks[i,0:self.config.LANDMARK_POINTS,1],landmarks[i,0:self.config.LANDMARK_POINTS,0]] = 1
                     outputs, gen_loss, dis_loss, logs = self.inpaint_model.process(images,landmark_map,masks)
-                    outputs_merged = (outputs * masks) + (images * (1-masks))
+                    outputs_merged = outputs
 
                     psnr = self.psnr(self.postprocess(images), self.postprocess(outputs_merged))
                     mae = (torch.sum(torch.abs(images - outputs_merged)) / torch.sum(images)).float()
